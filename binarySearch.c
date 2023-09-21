fe77a7590ff82cd8e07f986631e0e151a413d755
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-09-21 07:30:10 +0200

    Update binarySearch.c

--- binarySearch.c
+++ binarySearch.c
@@ -1,5 +1,13 @@
-#include<iostream>
-using namespace std;
+#include <stdio.h>
+
 int main(){
-  return 0;
+    int arr[10] = {0,1,2,3,4,5,6,7,8,9};
+    for ( int i = 0; i < 10; i++ ) {
+        printf("Element %i is %i \n",i, arr[i]);
+    }
+    int n = 3;
+//   do{
+//        
+//    }
+//    while()
 }
