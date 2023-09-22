c155e19c8c714dc9f41757f2413fface455c5154
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-09-22 08:01:38 +0200

    Update binarySearch.c

--- binarySearch.c
+++ binarySearch.c
@@ -2,12 +2,14 @@
 
 int main(){
     int arr[10] = {0,1,2,3,4,5,6,7,8,9};
+    int len = 10;
+    int target = 3;
     for ( int i = 0; i < 10; i++ ) {
         printf("Element %i is %i \n",i, arr[i]);
     }
-    int n = 3;
-//   do{
-//        
-//    }
-//    while()
+  // do{
+  //      //find the midpoint
+  //     int mid = len/2;
+  //  }
+  //  while()
 }
