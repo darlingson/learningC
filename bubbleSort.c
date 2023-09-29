5c2a1454eefd09f5f0a9c870fa1e625f83f9fd00
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-09-29 21:46:02 +0200

    completed bubble sort

--- bubbleSort.c
+++ bubbleSort.c
@@ -2,8 +2,22 @@
 
 int main(void){
     int arr[10] = {3,1,5,2,9,7,8,4,1,0};
-    int len = 10
+    int len = 10;
 
+    for(int i = 0; i < len ; i++){
+        //outer array
+        for(int j= i;j<len;j++){
+            //inner loop
+            if(arr[i] > arr[j]){
+                //swap the contents
+                int swap = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swap;
+            }
+        }
+    }
+
+    //print out the sorted array
     for (int i = 0; i < len ; i++){
         printf("%i",arr[i]);
     }
