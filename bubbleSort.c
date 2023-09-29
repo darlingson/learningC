865c26fdf47e06c802d72fb272544cf5506766d3
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-09-29 21:33:09 +0200

    updated bubbleSort.c

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
 
+int main(void){
+    int arr[10] = {3,1,5,2,9,7,8,4,1,0};
+    int len = 10
+
+    for (int i = 0; i < len ; i++){
+        printf("%i",arr[i]);
+    }
+    return 0;
+}
