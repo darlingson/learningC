38a4fbf1215450510aaf5e51eda73d6acbd531d7
Darlingson Makuwila <62630338+darlingson@users.noreply.github.com>
2023-10-30 14:00:11 +0200

    bst and dict

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -1,1 +1,25 @@
+typedef struct node{
+    int number;
+    struct node *left;
+    struct node *right;
+}
 
+bool search (node *tree,int number)
+{
+    if(tree == NULL)
+    {
+        return false;
+    }
+    else if (number < tree->number)
+    {
+        return search(tree->left,number);
+    }
+    else if(number > tree->number)
+    {
+        return search(tree->right, number);
+    }
+    else
+    {
+        return true;
+    }
+}
